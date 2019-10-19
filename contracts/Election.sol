2a2e1c23de1629658bf198098d81279f01872fcf
Karim Baccar <karim.baccar9@hotmail.com>
2019-10-20 01:50:53 +0200

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,47 @@
+pragma solidity 0.5.8;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+
+    mapping(address => bool) public voters;
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+    
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    // Constructor
+    constructor () public {
+      addCandidate("Candidate 1");
+      addCandidate("Candidate 2");
+      addCandidate("Candidate 3");
+      addCandidate("Candidate 4");
+      addCandidate("Candidate 5");
+    }
+}
